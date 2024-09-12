57f4249c5d51f396983d3993fb10741033b3a9be
leetaegeon <shfkfk9649@naver.com>
2024-09-12 13:36:16 +0900

    first commit

---
+++ src/main/java/com/sparta/spring26/Spring26Application.java
@@ -0,0 +1,13 @@
+package com.sparta.spring26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring26Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spring26/Spring26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring26ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
